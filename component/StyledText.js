554bc952f45b249e6bd6944f061082a96f83324e
vickyrajj <dev.vigneshraj.s@gmail.com>
2020-12-26 18:47:37 +0530

    asd

---
+++ component/StyledText.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function StyledText() {
+  return (
+    <View style={{}}>
+      <Text style={{fontSize:20,color:'#373737',fontWeight:'700'}}>NPM</Text>
+    </View>
+  );
+}
---
+++ dist/StyledText.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+export default function StyledText() {
+  return /*#__PURE__*/React.createElement(View, {
+    style: {}
+  }, /*#__PURE__*/React.createElement(Text, {
+    style: {
+      fontSize: 20,
+      color: '#373737',
+      fontWeight: '700'
+    }
+  }, "NPM"));
+}
