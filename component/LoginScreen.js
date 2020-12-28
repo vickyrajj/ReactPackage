f9e26ac62c94d11ccd4d5be3d6d4a54db3687a92
vickyrajj <dev.vigneshraj.s@gmail.com>
2020-12-28 14:48:28 +0530

    asdf

--- component/LoginScreen.js
+++ component/LoginScreen.js
@@ -13,64 +13,64 @@ import PropTypes from 'prop-types';
 
 const { width,height } = Dimensions.get('window');
 
-class Loader extends Component {
-  render() {
-    const { animationType, modalVisible } = this.props;
-    return (
-      <Modal
-        animationType={animationType}
-        transparent={true}
-        visible={modalVisible}
-      >
-        <View style={styles.wrapper}>
-          <View style={styles.loaderContainer}>
-            <Image
-              style={styles.loaderImage}
-              source={require("../assets/images/loader.gif")}
-            />
-          </View>
-        </View>
-      </Modal>
-    );
-  }
-}
-
-Loader.propTypes = {
-  animationType: PropTypes.string.isRequired,
-  modalVisible: PropTypes.bool.isRequired
-};
-const styles = StyleSheet.create({
-  wrapper: {
-    zIndex: 9,
-    backgroundColor: "rgba(0,0,0,0.3)",
-    position: "absolute",
-    width: "100%",
-    height: "100%",
-    top: 0,
-    left: 0
-  },
-  loaderContainer: {
-    width: 90,
-    height: 90,
-    backgroundColor: 'white',
-    borderRadius: 15,
-    position: "absolute",
-    left: "50%",
-    top: "50%",
-    marginLeft: -45,
-    marginTop: -45
-  },
-  loaderImage: {
-    width: 70,
-    height: 70,
-    borderRadius: 15,
-    position: "relative",
-    left: "50%",
-    marginLeft: -35,
-    top: "50%",
-    marginTop: -35
-  }
-});
+// class Loader extends Component {
+//   render() {
+//     const { animationType, modalVisible } = this.props;
+//     return (
+//       <Modal
+//         animationType={animationType}
+//         transparent={true}
+//         visible={modalVisible}
+//       >
+//         <View style={styles.wrapper}>
+//           <View style={styles.loaderContainer}>
+//             <Image
+//               style={styles.loaderImage}
+//               source={require("../assets/images/loader.gif")}
+//             />
+//           </View>
+//         </View>
+//       </Modal>
+//     );
+//   }
+// }
+//
+// Loader.propTypes = {
+//   animationType: PropTypes.string.isRequired,
+//   modalVisible: PropTypes.bool.isRequired
+// };
+// const styles = StyleSheet.create({
+//   wrapper: {
+//     zIndex: 9,
+//     backgroundColor: "rgba(0,0,0,0.3)",
+//     position: "absolute",
+//     width: "100%",
+//     height: "100%",
+//     top: 0,
+//     left: 0
+//   },
+//   loaderContainer: {
+//     width: 90,
+//     height: 90,
+//     backgroundColor: 'white',
+//     borderRadius: 15,
+//     position: "absolute",
+//     left: "50%",
+//     top: "50%",
+//     marginLeft: -45,
+//     marginTop: -45
+//   },
+//   loaderImage: {
+//     width: 70,
+//     height: 70,
+//     borderRadius: 15,
+//     position: "relative",
+//     left: "50%",
+//     marginLeft: -35,
+//     top: "50%",
+//     marginTop: -35
+//   }
+// });
 
 class LoginScreen extends Component {
 
@@ -432,7 +432,8 @@ class LoginScreen extends Component {
   }else{
     return(
       <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-        <Loader modalVisible = {true} animationType="fade" />
+        // <Loader modalVisible = {true} animationType="fade" />
+        <ActivityIndicator size="small" color="#3bb3c8" />
       </View>
     )
   }
