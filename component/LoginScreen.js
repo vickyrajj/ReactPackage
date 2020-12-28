7d2288c07d9192868cac4072368ee0e5c213cef2
vickyrajj <dev.vigneshraj.s@gmail.com>
2020-12-28 14:53:23 +0530

    asd

--- component/LoginScreen.js
+++ component/LoginScreen.js
@@ -13,65 +13,6 @@ import PropTypes from 'prop-types';
 
 const { width,height } = Dimensions.get('window');
 
-// class Loader extends Component {
-//   render() {
-//     const { animationType, modalVisible } = this.props;
-//     return (
-//       <Modal
-//         animationType={animationType}
-//         transparent={true}
-//         visible={modalVisible}
-//       >
-//         <View style={styles.wrapper}>
-//           <View style={styles.loaderContainer}>
-//             <Image
-//               style={styles.loaderImage}
-//               source={require("../assets/images/loader.gif")}
-//             />
-//           </View>
-//         </View>
-//       </Modal>
-//     );
-//   }
-// }
-//
-// Loader.propTypes = {
-//   animationType: PropTypes.string.isRequired,
-//   modalVisible: PropTypes.bool.isRequired
-// };
-// const styles = StyleSheet.create({
-//   wrapper: {
-//     zIndex: 9,
-//     backgroundColor: "rgba(0,0,0,0.3)",
-//     position: "absolute",
-//     width: "100%",
-//     height: "100%",
-//     top: 0,
-//     left: 0
-//   },
-//   loaderContainer: {
-//     width: 90,
-//     height: 90,
-//     backgroundColor: 'white',
-//     borderRadius: 15,
-//     position: "absolute",
-//     left: "50%",
-//     top: "50%",
-//     marginLeft: -45,
-//     marginTop: -45
-//   },
-//   loaderImage: {
-//     width: 70,
-//     height: 70,
-//     borderRadius: 15,
-//     position: "relative",
-//     left: "50%",
-//     marginLeft: -35,
-//     top: "50%",
-//     marginTop: -35
-//   }
-// });
-
 class LoginScreen extends Component {
 
     constructor(props) {
