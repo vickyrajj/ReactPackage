6114fbec095d05e298a390aad72c8c4eab30d022
vickyrajj <dev.vigneshraj.s@gmail.com>
2020-12-28 14:44:39 +0530

    asdf

--- component/LoginScreen.js
+++ component/LoginScreen.js
@@ -3,17 +3,74 @@ import React, { Component }  from 'react';
 import { Alert, ScrollView, StyleSheet, View, Text, TextInput, Picker,StatusBar, TouchableHighlight,TouchableOpacity, ImageBackground, Image,AsyncStorage,Keyboard,Linking,PermissionsAndroid,ToastAndroid,Dimensions} from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import Constants from 'expo-constants';
-import SmsListener from 'react-native-android-sms-listener'
 import GradientButton from "react-native-gradient-buttons";
 import { FontAwesome } from '@expo/vector-icons';
 import * as Expo from 'expo';
 import * as Permissions from 'expo-permissions';
-import Loader from '../components/Loader';
 import Svg, { Circle, Rect,Path,Defs,G,Mask} from 'react-native-svg';
 import Toast, {DURATION} from 'react-native-easy-toast';
 import PropTypes from 'prop-types';
 
 const { width,height } = Dimensions.get('window');
+
+class Loader extends Component {
+  render() {
+    const { animationType, modalVisible } = this.props;
+    return (
+      <Modal
+        animationType={animationType}
+        transparent={true}
+        visible={modalVisible}
+      >
+        <View style={styles.wrapper}>
+          <View style={styles.loaderContainer}>
+            <Image
+              style={styles.loaderImage}
+              source={require("../assets/images/loader.gif")}
+            />
+          </View>
+        </View>
+      </Modal>
+    );
+  }
+}
+
+Loader.propTypes = {
+  animationType: PropTypes.string.isRequired,
+  modalVisible: PropTypes.bool.isRequired
+};
+const styles = StyleSheet.create({
+  wrapper: {
+    zIndex: 9,
+    backgroundColor: "rgba(0,0,0,0.3)",
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    top: 0,
+    left: 0
+  },
+  loaderContainer: {
+    width: 90,
+    height: 90,
+    backgroundColor: 'white',
+    borderRadius: 15,
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    marginLeft: -45,
+    marginTop: -45
+  },
+  loaderImage: {
+    width: 70,
+    height: 70,
+    borderRadius: 15,
+    position: "relative",
+    left: "50%",
+    marginLeft: -35,
+    top: "50%",
+    marginTop: -35
+  }
+});
 
 class LoginScreen extends Component {
 
